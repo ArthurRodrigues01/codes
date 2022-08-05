c5c762f0eb6edb7fa2e4c6184cd81ff98c67758a
ArthurRodrigues <88101116+ArthurRodrigues01@users.noreply.github.com>
2022-08-05 16:17:47 -0300

    Update lista-de-compras.js

--- web/lista-de-compras/lista-de-compras.js
+++ web/lista-de-compras/lista-de-compras.js
@@ -1,11 +1,13 @@
 const listItemContainer = document.querySelector(".list-item-container")
+const eraseButton = document.querySelectorAll(".erase-button")
 const submitButton = document.querySelector("#submit-button")
-
+var cont = 0
 submitButton.addEventListener("click", () => {
   let inputItems = document.querySelector("#submit-items")
   if(inputItems.value != ""){
-    listItemContainer.innerHTML += `<div><img><h1 class="list-item-title stripped">${inputItems.value}</h1></div>`
+    listItemContainer.innerHTML += `<div><h1 class="list-item-title stripped" id="${cont}">${inputItems.value}<img src="x.svg" class="erase-button" id="erase-button-${cont}"></h1></div>`
     inputItems.value = ""
+    cont++
   }
 })
 
@@ -17,3 +19,11 @@ listItemContainer.addEventListener("DOMNodeInserted", () => {
     }
   }  
 })
+
+eraseButton.forEach(element => element.addEventListener("click", () => {
+    //let xButton = document.querySelector(`#erase-button-${element.id}`)
+    
+    //xButton.remove()
+    console.log(element.id)
+  
+}))
