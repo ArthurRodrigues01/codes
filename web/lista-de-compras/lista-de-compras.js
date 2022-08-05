6cae7770550b4d65039064883adc4089aa285823
ArthurRodrigues <88101116+ArthurRodrigues01@users.noreply.github.com>
2022-08-05 12:03:50 -0300

    Update lista-de-compras.js

--- web/lista-de-compras/lista-de-compras.js
+++ web/lista-de-compras/lista-de-compras.js
@@ -1,11 +1,19 @@
 const listItemContainer = document.querySelector(".list-item-container")
-
 const submitButton = document.querySelector("#submit-button")
 
 submitButton.addEventListener("click", () => {
   let inputItems = document.querySelector("#submit-items")
   if(inputItems.value != ""){
-    listItemContainer.innerHTML += `<div><h3>${inputItems.value}</h3></div>`
+    listItemContainer.innerHTML += `<div><img><h1 class="list-item-title stripped">${inputItems.value}</h1></div>`
     inputItems.value = ""
   }
+})
+
+listItemContainer.addEventListener("DOMNodeInserted", () => {
+  let stripped = document.querySelectorAll(".stripped")
+  for(i = 0; i < stripped.length; i++){
+    if(i % 2 != 0){
+      stripped[i].style.backgroundColor = "#ddd"
+    }
+  }  
 })
