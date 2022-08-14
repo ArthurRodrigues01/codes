68b6aaf7ded7b3a5914f43089fb16235bc29fcad
Arthur Rodrigues <art.costa.2005@gmail.com>
2022-08-14 12:30:31 -0300

    add

---
+++ js/concatenating-array-of-objects.js
@@ -0,0 +1,70 @@
+/*************************************************************************
+*
+* Author: Arthur Rodrigues
+*
+* O que o programa faz? 
+*   O programa gera um array de objetos, com duas propriedades, 
+*   um id(aleatório) e uma descrição, a descrição é inicialmente nula, mas
+*   o programa substituí o valor da descrição de elementos específicos.
+* Finalidade do programa: 
+*   Testar o novo operador '...', em relação ao método 'concat'.
+*
+*************************************************************************/
+
+var users
+
+function generateUUID() { // Public Domain/MIT
+  var d = new Date().getTime();//Timestamp
+  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16;//random number between 0 and 16
+    if (d > 0) {//Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {//Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
+
+function isElementInArray(element, array){
+  for(f = 0; f < array.length; f++){
+    if(array[f].id === element.id){
+      return true;
+    }
+  }
+  return false;
+}
+
+function getDummyObjectsArray(){
+  let array
+  for(i = 0; i < 10; i++){
+    if(array === undefined){
+      array = [{id: generateUUID(), description: ""}]
+    }else{
+      array = [...array, {id: generateUUID(), description: ""}]
+    }
+  }
+  return array
+}
+function main(){
+  users = getDummyObjectsArray()
+  isEditable = true
+  editableIDs = [users[2], users[5], users[9]]
+  
+  if(isEditable){
+    users.map(item => {
+      if(isElementInArray(item, editableIDs)){
+        item.description = "Valor alterado"
+      }
+    })
+  }
+  
+  users.map(item => {
+    console.log(item)
+  })
+}
+
+main()
---
+++ js/sorting-system.js
@@ -0,0 +1,77 @@
+/*************************************************************************
+*
+* Author: Arthur Rodrigues
+*
+* O que o programa faz?
+* 	O programa ordena strings com base na tabela ASCII.
+* Finalidade do programa:
+*		Aprendizado.
+*
+*************************************************************************/
+
+
+var words = ["João", "Paulo", "Pedro", "Gabriela", "Davi", "Teresinha", "Matteus", "Matheus", "Arthur", "Abraão", "Zack", "Cold"]
+var orderedWords = []
+
+function getSmallest(word1, word2) {
+	if (word1.length < word2.length) {
+		return word1
+	}
+	return word2
+}
+
+function getFirstInOrder(word1, word2) {
+	let smallestWord = getSmallest(word1, word2)
+	let word1Lowered = word1.toLowerCase()
+	let word2Lowered = word2.toLowerCase()
+	let boolString1 = 0
+	let boolString2 = 0
+	for (f = 0; f < smallestWord.length; f++) {
+		if (word1Lowered[f] < word2Lowered[f] || word1Lowered[f] == word2Lowered[f]) {
+			boolString1++
+		} else {
+			break
+		}
+	}
+	for (f = 0; f < smallestWord.length; f++) {
+		if (word2Lowered[f] < word1Lowered[f] || word2Lowered[f] == word1Lowered[f]) {
+			boolString2++
+		} else {
+			break
+		}
+	}
+
+	if (boolString1 > boolString2) {
+		return word1
+	} else if (boolString2 > boolString1) {
+		return word2
+	}
+	return smallestWord
+}
+
+function main() {
+	let string1
+	let string2
+	while (words.length != 1) {
+		for (i = 0; i < words.length; i++) {
+			if (i == 0) {
+				string1 = words[0]
+				string2 = words[1]
+				string1 = getFirstInOrder(string1, string2)
+			} else {
+				string2 = words[i]
+				string1 = getFirstInOrder(string1, string2)
+			}
+		}
+		words.splice(words.indexOf(string1), 1)
+		orderedWords.push(string1)
+	}
+	i = 0
+	orderedWords.push(words[0])
+	orderedWords.map(item => {
+		i++
+		console.log(`${i} - ${item}`)
+	})
+}
+
+main()
